fc336c8430e46bbf0f77b72c9726bd8228278aed
shekiat <114537094+shekiat@users.noreply.github.com>
2023-11-09 15:16:57 -0500

    Add files via upload
    - had an error when running this that said: PyQt5.QtWidgets import *
    ModuleNotFoundError: No module named 'PyQt5'
    - already have pyqt5 installed, so I'm researching why this is happening.

---
+++ basic_ui.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+class MyGUI(QMainWindow):
+    def __init__(self):
+        super(MyGUI, self), __init__()
+        uic.loadUi("my_ui.ui", self)
+        self.show()
+
+        self.pushButton.clicked.connect(self.submit)
+
+    def submit(self):
+        if self.lineEdit_3.text() == "text":
+            self.lineEdit.setEnabled(True)
+            self.lineEdit_2.setEnabled(True)
+        else:
+            message = QMessageBox()
+            message.setText("Invalid API Key")
+            message.exec_()
+
+def main():
+    app = QApplication([])
+    window = MyGUI()
+    app.exec_()
+
+if __name__ == "__main__":
+    main
